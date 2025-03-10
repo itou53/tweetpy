bed0620e964f5351ffc74128acce1d936f34fa6f
itou53 <131519728+itou53@users.noreply.github.com>
2025-03-11 00:27:48 +0900

    openai turtorial

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import os
 from openai import OpenAI
 
-def lambda_handler(event, context):
-    client = OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
-    )
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "Say this is a test",
-            }
-        ],
-        model="gpt-4o",
-    )
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "あなたは新設なアシスタントです。"},
+        {
+            "role": "user",
+            "content": "プログラミングにおける再起について俳句を書いてください"
+        }
+    ]
+)
 
-    response_context = chat_completion.choices[0].message.content
-    print(response_context)
+print(completion.choices[0].message.content)
